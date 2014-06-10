a978c7ddde1a48b5c0ad91452cd8591aba148893
Yu Xiang <xyfudan@gmail.com>
2014-06-10 15:18:53 -0700

    evaluate DPM

---
+++ 3rd_party/voc-release4.01/display_result_kitti.m
@@ -0,0 +1,52 @@
+function display_result_kitti(cls)
+
+% read detection results
+filename = sprintf('data/%s_test.mat', cls);
+object = load(filename);
+dets = object.dets;
+
+% read ids of validation images
+object = load('kitti_ids.mat');
+ids = object.ids_val;
+N = numel(ids);
+
+% KITTI path
+globals;
+root_dir = KITTIroot;
+data_set = 'training';
+cam = 2;
+image_dir = fullfile(root_dir,[data_set '/image_' num2str(cam)]);
+
+figure;
+for i = 1:N
+    det = dets{i};
+    num = size(det, 1);
+    
+    img_idx = ids(i);
+    file_img = sprintf('%s/%06d.png',image_dir, img_idx);
+    I = imread(file_img);
+    
+    if i ~= 1 && mod(i-1, 8) == 0
+        pause;
+    end
+    ind = mod(i-1,8)+1;
+    
+    subplot(4, 2, ind);
+    imshow(I);
+    hold on;
+
+    til = sprintf('%d', i);
+    for k = 1:min(5, num)
+        % get predicted bounding box
+        bbox_pr = det(k,1:4);
+        bbox_draw = [bbox_pr(1), bbox_pr(2), bbox_pr(3)-bbox_pr(1), bbox_pr(4)-bbox_pr(2)];
+        rectangle('Position', bbox_draw, 'EdgeColor', 'g', 'LineWidth',2);
+        text(bbox_pr(1), bbox_pr(2), num2str(k), 'FontSize', 16, 'BackgroundColor', 'r');
+        
+
+        til = sprintf('%s, s%d=%.2f', til, k, det(k,6));        
+    end
+    title(til);
+    subplot(4, 2, ind);
+    hold off;
+end
