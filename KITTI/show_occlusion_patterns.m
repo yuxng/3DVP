55a751d1c07ed7f0f9b1ae5a6fb4074403fa89ed
Yu Xiang <xyfudan@gmail.com>
2014-06-20 17:51:10 -0700

    show occlusion patterns from annotation files

--- KITTI/show_occlusion_patterns.m
+++ KITTI/show_occlusion_patterns.m
@@ -1,15 +1,9 @@
-function show_occlusion_patterns(idx)
+function show_occlusion_patterns
 
-% load occlusion patterns
-object = load('occlusion_patterns.mat');
-grid = object.grid;
-N = size(grid, 1);
-
-if nargin < 1
-    idx = 1:N;
-else
-    idx = unique(idx);
-end
+% annotations
+path_ann = 'Annotations';
+files = dir(fullfile(path_ann, '*.mat'));
+N = numel(files);
 
 % load the mean CAD model
 cls = 'car';
@@ -17,37 +11,28 @@ filename = sprintf('../Geometry/%s_mean.mat', cls);
 object = load(filename);
 cad = object.(cls);
 
-% define the azimuth and elevation for visibility testing
-a = 0:45:315;
-e = 15;
-vnum = numel(a);
-per = size(vnum, 1);
-visibility_grid = cell(vnum, 1);
-num_total = zeros(vnum, 1);
-for i = 1:vnum
-    visibility_grid{i} = check_visibility(cad, a(i), e);
-    tmp = visibility_grid{i} == 1;
-    visibility_grid{i} = reshape(tmp, 1, numel(tmp));
-    num_total(i) = sum(visibility_grid{i});
-end
-
 figure;
-for i = 1:numel(idx)
-    if i ~= 1 && mod(i-1, 16) == 0
-        pause;
-    end
-    ind = mod(i-1, 16)+1;
+ind_plot = 1;
+for i = 1:N
+    % load annotation
+    filename = fullfile(path_ann, files(i).name);
+    object = load(filename);
+    record = object.record;
+    objects = record.objects;
     
-    pattern = grid(idx(i),:);
-    % find the azimuth to show the pattern
-    for j = 1:vnum
-        num_occluded = sum(visibility_grid{j} & pattern);
-        per(j) = num_occluded / num_total(j);
+    for j = 1:numel(objects)
+        object = objects(j);
+        if strcmp(object.type, 'Car') == 1 && object.occ_per > 0.9
+            subplot(4, 4, ind_plot);
+            draw_cad(cad, object.grid);
+            view(object.azimuth, object.elevation);
+            til = sprintf('%s, object %d, occ=%.2f', files(i).name, j, object.occ_per);
+            title(til);
+            ind_plot = ind_plot + 1;
+            if ind_plot > 16
+                ind_plot = 1;
+                pause;
+            end
+        end
     end
-    tmp = abs(per - 0.5);
-    [~, index] = min(tmp);
-    
-    subplot(4, 4, ind);
-    draw_cad(cad, pattern);
-    view(a(index), e);
 end
