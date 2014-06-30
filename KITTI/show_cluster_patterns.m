bc22945bdd61357e473edf640228931ca636ec1c
Yu Xiang <yuxiang@capri5.Stanford.EDU>
2014-06-30 10:32:36 -0700

    clustering with non-occluded objects

--- KITTI/show_cluster_patterns.m
+++ KITTI/show_cluster_patterns.m
@@ -2,6 +2,12 @@ function show_cluster_patterns(idx)
 
 opt = globals;
 
+% get image directory
+root_dir = opt.path_kitti_root;
+data_set = 'training';
+cam = 2; % 2 = left color camera
+image_dir = fullfile(root_dir, [data_set '/image_' num2str(cam)]);
+
 % load data
 object = load('data.mat');
 data = object.data;
@@ -19,14 +25,26 @@ N = numel(centers);
 
 figure;
 for i = 1:N
+    ind_plot = 1;
     % show center
     ind = centers(i);
     grid = data.grid(:,ind);
     visibility_grid = zeros(size(cad.grid));
     visibility_grid(index) = grid;
-    subplot(4, 4, 1);
+    subplot(4, 8, ind_plot);
+    ind_plot = ind_plot + 1;
     draw_cad(cad, visibility_grid);
-    view(data.azimuth(ind), data.elevation(ind));    
+    view(data.azimuth(ind), data.elevation(ind));
+    
+    % show the image patch
+    filename = fullfile(image_dir, data.imgname{ind});
+    I = imread(filename);
+    bbox = data.bbox(:,ind);
+    rect = [bbox(1) bbox(2) bbox(3)-bbox(1) bbox(4)-bbox(2)];
+    I1 = imcrop(I, rect);
+    subplot(4, 8, ind_plot);
+    ind_plot = ind_plot + 1;
+    imshow(I1);
     
     % show several members
     member = find(idx == ind);
@@ -37,9 +55,20 @@ for i = 1:N
         grid = data.grid(:,ind);
         visibility_grid = zeros(size(cad.grid));
         visibility_grid(index) = grid;
-        subplot(4, 4, 1+j);
+        subplot(4, 8, ind_plot);
+        ind_plot = ind_plot + 1;
         draw_cad(cad, visibility_grid);
-        view(data.azimuth(ind), data.elevation(ind));        
+        view(data.azimuth(ind), data.elevation(ind));
+        
+        % show the image patch
+        filename = fullfile(image_dir, data.imgname{ind});
+        I = imread(filename);
+        bbox = data.bbox(:,ind);
+        rect = [bbox(1) bbox(2) bbox(3)-bbox(1) bbox(4)-bbox(2)];
+        I1 = imcrop(I, rect);
+        subplot(4, 8, ind_plot);
+        ind_plot = ind_plot + 1;
+        imshow(I1);        
     end
     pause;
 end
