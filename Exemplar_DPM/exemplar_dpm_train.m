15063b133f007a432e6db5f59c8f632a5e42dc54
Yu Xiang <yuxiang@capri5.Stanford.EDU>
2014-07-04 15:10:02 -0700

    add more paths into exemplar dpm globals

--- Exemplar_DPM/exemplar_dpm_train.m
+++ Exemplar_DPM/exemplar_dpm_train.m
@@ -1,12 +1,13 @@
 function exemplar_dpm_train
 
+cls = 'car';
+
 % Main function to train exemplar DPM for occlusion patterns
 
 % load the mean CAD model
-cls = 'car';
-filename = sprintf('../Geometry/%s_mean.mat', cls);
-object = load(filename);
-cad = object.(cls);
+% filename = sprintf('../Geometry/%s_mean.mat', cls);
+% object = load(filename);
+% cad = object.(cls);
 
 % load occlusion patterns
 filename = '../KITTI/data.mat';
@@ -18,9 +19,10 @@ centers = unique(data.idx);
 
 % train an exemplar DPM for each cluster
 num = numel(centers);
-for i = 1:1
+
+for i = 1:num
     fprintf('%d/%d: Train DPM for center %d\n', i, num, centers(i));
-    exemplar_show_pattern(data, cad, centers(i));
-    pause(1);
+%     exemplar_show_pattern(data, cad, centers(i));
+%     pause(1);
     kitti_train(cls, data, centers(i), '');
 end
