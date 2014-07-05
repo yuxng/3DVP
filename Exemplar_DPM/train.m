0cc0e7aa5740acdc4bbae8baf7800a7fe6939690
Yu Xiang <yuxiang@capri5.Stanford.EDU>
2014-07-04 21:56:59 -0700

    remove keyboard input in train.m

--- Exemplar_DPM/train.m
+++ Exemplar_DPM/train.m
@@ -129,7 +129,6 @@ for t = 1:iter
       end
       if t > 1 && pos_loss(t,2) > pos_loss(t,1)
         fprintf('warning: pos loss went up\n');
-        keyboard;
       end
       % stop if relabeling doesn't reduce the positive loss by much
       if (t > 1) && (pos_loss(t,2)/pos_loss(t,1) > 0.999)
