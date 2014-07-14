a0eec91f36205f196e5b93c594027ea6389934a0
Yu Xiang <xyfudan@gmail.com>
2014-07-14 10:29:49 -0700

    tmp

--- Exemplar_DPM/exemplar_combine_detections.m
+++ Exemplar_DPM/exemplar_combine_detections.m
@@ -14,11 +14,24 @@ N = numel(centers);
 dets = [];
 for i = 1:N
     cid = centers(i);
+    
+    % compute the range of this cluster
+    bbox = data.bbox(:, idx == cid);
+    x = (bbox(1,:) + bbox(3,:)) / 2;
+    y = (bbox(2,:) + bbox(4,:)) / 2;
+    w = bbox(3,:) - bbox(1,:);
+    h = bbox(4,:) - bbox(2,:);
+    r = [x; y; w; h];
+    rmin = min(r, [], 2);
+    rmax = max(r, [], 2);
+    rlen = rmax - rmin;
+    lim = [rmin - 0.1*rlen, rmax + 0.1*rlen];
+    
     filename = sprintf('kitti_train/%s_%d_test.mat', cls, cid);
     fprintf('load %s\n', filename);
     object = load(filename);
     boxes1 = object.boxes1;
-    boxes_new = cellfun(@(x) process_boxes(x, cid, threshold), boxes1, 'UniformOutput', false);
+    boxes_new = cellfun(@(x) process_boxes(x, cid, threshold, lim), boxes1, 'UniformOutput', false);
     if i == 1
         dets = boxes_new;
     else
@@ -30,12 +43,18 @@ filename = sprintf('kitti_train/%s_test.mat', cls);
 save(filename, 'dets', '-v7.3');
 
 
-function boxes_new = process_boxes(boxes, cid, threshold)
+function boxes_new = process_boxes(boxes, cid, threshold, lim)
 
 if isempty(boxes) == 1
     boxes_new = [];
 else
-    index= find(boxes(:, 5) > threshold);
+    x = (boxes(:,1) + boxes(:,3)) / 2;
+    y = (boxes(:,2) + boxes(:,4)) / 2;
+    w = boxes(:,3) - boxes(:,1);
+    h = boxes(:,4) - boxes(:,2);
+    
+    index= find((boxes(:, 5) > threshold & x > lim(1,1) & x < lim(1,2) & ...
+        y > lim(2,1) & y < lim(2,2) & w > lim(3,1) & w < lim(3,2) & h > lim(4,1) & h < lim(4,2)) == 1);
     if isempty(index) == 1
         boxes_new = [];
     else
