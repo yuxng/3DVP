495bb5d3d5fa91940fb168eefcf728854f5796cc
Yu Xiang <yuxiang@capri5.Stanford.EDU>
2014-08-16 03:21:05 -0700

    evaluate ACF

---
+++ ACF/exemplar_combine_detections.m
@@ -0,0 +1,105 @@
+function exemplar_combine_detections
+
+cls = 'car';
+threshold = -inf;
+is_train = 1;
+is_calibration = 0;
+
+% load data
+if is_train == 1
+    object = load('../KITTI/data.mat');
+else
+    object = load('../KITTI/data_all.mat');
+end
+data = object.data;
+idx = data.idx_ap;
+centers = double(unique(idx));
+N = numel(centers);
+
+% load detections
+dets = [];
+for i = 1:N
+    cid = centers(i);
+    num = numel(find(idx == cid));
+    fprintf('cluster %d: %d training examples\n', cid, num);
+    
+    % compute the range of this cluster
+    % compute the range of bounding box center, width and height
+    bbox = data.bbox(:, idx == cid);
+    x = (bbox(1,:) + bbox(3,:)) / 2;
+    y = (bbox(2,:) + bbox(4,:)) / 2;
+    w = bbox(3,:) - bbox(1,:);
+    h = bbox(4,:) - bbox(2,:);
+    r = [x; y; w; h];
+    rmin = min(r, [], 2);
+    rmax = max(r, [], 2);
+    rlen = rmax - rmin;
+    lim = [rmin - 0.1*rlen rmax + 0.1*rlen];
+    
+    if is_train == 1
+        filename = sprintf('kitti_train/%s_%d_test.mat', cls, cid);
+    else
+        filename = sprintf('kitti_test/%s_%d_test.mat', cls, cid);
+    end
+    object = load(filename);
+    boxes1 = object.boxes;
+    
+    % load the calibration weights
+    if is_calibration == 1
+        if is_train == 1
+            filename = sprintf('kitti_train/%s_%d_calib.mat', cls, cid);
+        else
+            filename = sprintf('kitti_test/%s_%d_calib.mat', cls, cid);
+        end
+        if exist(filename, 'file') == 0
+            beta = [];
+        else
+            object = load(filename);
+            beta = object.beta;
+        end
+    else
+        beta = [];
+    end
+    
+    boxes_new = cellfun(@(x) process_boxes(x, cid, threshold, lim, beta), boxes1, 'UniformOutput', false);
+    if isempty(dets) == 1
+        dets = boxes_new;
+    else
+        dets = cellfun(@(x,y) [x; y], dets, boxes_new, 'UniformOutput', false);
+    end
+end
+
+if is_train == 1
+    filename = sprintf('kitti_train/%s_test.mat', cls);
+else
+    filename = sprintf('kitti_test/%s_test.mat', cls);
+end
+save(filename, 'dets', '-v7.3');
+
+
+function boxes_new = process_boxes(boxes, cid, threshold, lim, beta)
+
+if isempty(boxes) == 1
+    boxes_new = [];
+else
+    boxes = [boxes(:,1) boxes(:,2) boxes(:,1)+boxes(:,3) boxes(:,2)+boxes(:,4) boxes(:,5)];
+    x = (boxes(:,1) + boxes(:,3)) / 2;
+    y = (boxes(:,2) + boxes(:,4)) / 2;
+    w = boxes(:,3) - boxes(:,1);
+    h = boxes(:,4) - boxes(:,2);    
+    
+    index= find((boxes(:, 5) > threshold & x > lim(1,1) & x < lim(1,2) & ...
+        y > lim(2,1) & y < lim(2,2) & w > lim(3,1) & w < lim(3,2) & h > lim(4,1) & h < lim(4,2)) == 1);
+    if isempty(index) == 1
+        boxes_new = [];
+    else
+        tmp = boxes(index,:);
+        scores = tmp(:,5);
+        if isempty(beta) == 1
+            s = scores;
+        else
+            s = 1./(1+exp(-beta(1)*(scores-beta(2))));
+        end
+        boxes_new = [tmp(:,1:4) cid*ones(size(index)) s];
+    end
+end
---
+++ ACF/exemplar_write_kitti_results.m
@@ -0,0 +1,92 @@
+function exemplar_write_kitti_results
+
+cls = 'car';
+is_train = 1;
+
+% read detection results
+if is_train == 1
+    filename = sprintf('kitti_train/%s_test.mat', cls);
+else
+    filename = sprintf('kitti_test/%s_test.mat', cls);
+end
+object = load(filename);
+dets = object.dets;
+fprintf('load detection done\n');
+
+% load clustering data
+if is_train == 1
+    object = load('../KITTI/data.mat');
+else
+    object = load('../KITTI/data_trainval.mat');
+end
+data = object.data;
+
+% read ids of validation images
+object = load('kitti_ids.mat');
+if is_train == 1
+    ids = object.ids_val;
+else
+    ids = object.ids_test;
+end
+N = numel(ids);
+
+for i = 1:N
+    img_idx = ids(i);    
+    det = dets{img_idx + 1};
+    
+    % result file
+    if is_train == 1
+        filename = sprintf('results_kitti_train/%06d.txt', img_idx);
+    else
+        filename = sprintf('results_kitti_test/%06d.txt', img_idx);
+    end
+    disp(filename);
+    fid = fopen(filename, 'w');
+    
+    if isempty(det) == 1
+        fprintf('no detection for image %d\n', img_idx);
+        fclose(fid);
+        continue;
+    end
+    
+    % non-maximum suppression
+    if isempty(det) == 0
+        I = nms(det, 0.5);
+        det = det(I, :);    
+    end    
+    
+    % write detections
+    num = size(det, 1);
+    for k = 1:num
+        cid = det(k, 5);
+        truncation = data.truncation(cid);
+        
+        occ_per = data.occ_per(cid);
+        if occ_per > 0.5
+            occlusion = 2;
+        elseif occ_per > 0
+            occlusion = 1;
+        else
+            occlusion = 0;
+        end
+        
+        azimuth = data.azimuth(cid);
+        alpha = azimuth + 90;
+        if alpha >= 360
+            alpha = alpha - 360;
+        end
+        alpha = alpha*pi/180;
+        if alpha > pi
+            alpha = alpha - 2*pi;
+        end
+        
+        h = data.h(cid);
+        w = data.w(cid);
+        l = data.l(cid);
+        
+        fprintf(fid, '%s %f %d %f %f %f %f %f %f %f %f %f %f %f %f %f\n', ...
+            'Car', truncation, occlusion, alpha, det(k,1), det(k,2), det(k,3), det(k,4), ...
+            h, w, l, -1, -1, -1, -1, det(k,6));
+    end
+    fclose(fid);
+end
