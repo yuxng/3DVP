01788b9ec98c1e5db489512b4b66cc72a5082b04
Yu Xiang <yuxiang@capri5.Stanford.EDU>
2014-09-27 15:19:04 -0700

    2d kmeans 40

--- ACF/qsub/exemplar_make_pbs_scripts.m
+++ ACF/qsub/exemplar_make_pbs_scripts.m
@@ -9,7 +9,7 @@ cids = unique(data.idx_kmeans);
 cids(cids == -1) = [];
 num = numel(cids);
 
-is_multiple = 0;
+is_multiple = 1;
 if is_multiple
     num_job = 32;
     index = round(linspace(1, num, num_job+1));
