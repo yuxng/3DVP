d9c3a92b714ca9e17351f9434acf8fa4114fb438
Yu Xiang <yuxiang@capri5.Stanford.EDU>
2014-09-27 17:15:44 -0700

    handle one positive in ACF

--- ACF/exemplar_acf_train.m
+++ ACF/exemplar_acf_train.m
@@ -327,8 +327,13 @@ fprintf('Sampled %i windows from %i images.\n',k,i);
 
 % optionally jitter positive windows
 if length(Is) < 2
-    Is{2} = Is{1};
-    Is = Is';
+    if positive
+        Is{2} = Is{1};
+        Is = Is';
+    else
+        Is = {};
+        return;
+    end
 end
 nd = ndims(Is{1}) + 1;
 Is = cat(nd,Is{:});
