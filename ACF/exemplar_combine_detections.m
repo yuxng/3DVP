7a643c00bd0aff15f5e642505f6a457462f8b10d
Yu Xiang <yuxiang@capri5.Stanford.EDU>
2014-09-28 20:00:25 -0700

    3d kmeans 200

--- ACF/exemplar_combine_detections.m
+++ ACF/exemplar_combine_detections.m
@@ -5,7 +5,7 @@ threshold = -inf;
 is_train = 1;
 is_calibration = 0;
 is_filtering = 1;
-result_dir = 'kitti_train_kmeans_3d_100';
+result_dir = 'kitti_train_kmeans_3d_150';
 
 % load data
 if is_train == 1
