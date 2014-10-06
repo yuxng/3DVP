6a5a3fd4fd1ba97ee92673f74f9be946eaf6443f
Yu Xiang <yuxiang@capri5.Stanford.EDU>
2014-10-05 17:45:12 -0700

    fix a bug in channel feature

--- ACF/chnsPyramid.m
+++ ACF/chnsPyramid.m
@@ -22,7 +22,7 @@ function pyramid = chnsPyramid( I, varargin )
 % can be used to approximate feature responses at nearby scales. The
 % approximation is accurate at least within an entire scale octave. For
 % details and to understand why this unexpected result holds, please see:
-%   P. Dollár, R. Appel, S. Belongie and P. Perona
+%   P. Dollï¿½r, R. Appel, S. Belongie and P. Perona
 %   "Fast Feature Pyramids for Object Detection", PAMI 2014.
 %
 % The parameter "nApprox" determines how many intermediate scales are
@@ -187,11 +187,23 @@ end
 function [scales,scaleshw] = getScales(nPerOct,nOctUp,minDs,shrink,sz)
 % set each scale s such that max(abs(round(sz*s/shrink)*shrink-sz*s)) is
 % minimized without changing the smaller dim of sz (tricky algebra)
-if(any(sz==0)), scales=[]; scaleshw=[]; return; end
-nScales = floor(nPerOct*(nOctUp+log2(min(sz./minDs)))+1);
+if(any(sz==0))
+    scales=[];
+    scaleshw=[];
+    return;
+end
+
+nScales = max(floor(nPerOct*(nOctUp+log2(min(sz./minDs)))+1), 1);
 scales = 2.^(-(0:nScales-1)/nPerOct+nOctUp);
-if(sz(1)<sz(2)), d0=sz(1); d1=sz(2); else d0=sz(2); d1=sz(1); end
-for i=1:nScales, s=scales(i);
+if(sz(1)<sz(2))
+    d0=sz(1);
+    d1=sz(2);
+else
+    d0=sz(2);
+    d1=sz(1);
+end
+for i=1:nScales
+    s=scales(i);
   s0=(round(d0*s/shrink)*shrink-.25*shrink)./d0;
   s1=(round(d0*s/shrink)*shrink+.25*shrink)./d0;
   ss=(0:.01:1-eps)*(s1-s0)+s0;
@@ -199,7 +211,8 @@ for i=1:nScales, s=scales(i);
   es1=d1*ss; es1=abs(es1-round(es1/shrink)*shrink);
   [~,x]=min(max(es0,es1)); scales(i)=ss(x);
 end
-kp=[scales(1:end-1)~=scales(2:end) true]; scales=scales(kp);
+kp = [scales(1:end-1) ~= scales(2:end) true];
+scales=scales(kp);
 scaleshw = [round(sz(1)*scales/shrink)*shrink/sz(1);
   round(sz(2)*scales/shrink)*shrink/sz(2)]';
 end
