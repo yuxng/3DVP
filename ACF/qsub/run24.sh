eac462f6a9bb8d6d3b48f51280a30c3120d3ce00
Yu Xiang <yuxiang@capri5.Stanford.EDU>
2014-10-06 18:15:37 -0700

    add sh

---
+++ ACF/qsub/run24.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#PBS -S /bin/bash
+#PBS -N run_it24
+#PBS -l nodes=1:ppn=12
+#PBS -l mem=2gb
+#PBS -l walltime=48:00:00
+#PBS -q cvgl
+echo "I ran on:"
+cat $PBS_NODEFILE
+cd /scail/scratch/u/yuxiang/SLM/ACF
+matlab.new -nodesktop -nosplash -r "exemplar_dpm_train_and_test(45:46); exit;"
